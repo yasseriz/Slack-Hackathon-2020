6cad5150370426e2694dda2baeda347217dea72c
yasseriz <yasserizaheer@gmail.com>
2020-03-08 17:56:06 +0800

    Obtained user input

--- waterBot.py
+++ waterBot.py
@@ -1,173 +1,223 @@
 from flask import request, Flask
 import os
 import slack
+from threading import Thread
+import json
 
-SLACK_OAUTH_TOKEN = 'xoxp-974374833842-975694945859-986809404900-6552916ad83acf3d3f217bc2b3b33882'
-SLACK_BOT_USER_TOKEN = 'xoxb-974374833842-975733718883-cU3GDLRlhHorebv82p82lmL7'
+
 
 Client = slack.WebClient(SLACK_BOT_USER_TOKEN)
 
 app = Flask(__name__)
 
+def backgroundworker(payload, userID,checkApp, userName):
+	print(payload)
+
+	workingDuration = payload['view']['state']['values']['workinghours']['workinghours']
+	selectedDays = payload['view']['state']['values']['days']['days']['selected_option']
+	duration = payload['view']['state']['values']['duration']['duration']['selected_option']['text']['text']
+
+	message = " I've got your request, I shall remind you to drink water on" + "\n " + selectedDays + "\n every" +  duration + "\n during " + workingDuration
+	Client.chat_postMessage(channel='general', text=message)
+
 @app.route('/slack/getco', methods=['POST'])
 def getco():
-    return "",200
+
+	print("Received something")
+	print(request.form)
+	payload = json.loads(request.form.get('payload'))
+	checkApp = payload['view']['title']['text']
+	userID = payload['user']['id']
+	userName = payload['user']['username']
+	
+	thr = Thread(target=backgroundworker, args=[payload, userID, checkApp, userName])
+	thr.start()
+	return "",200
 
 @app.route('/remindme', methods=['POST'])
 def remindme():
-    Client.chat_postMessage(channel='general', text='TEST')
-    return "",200
+	trigger_id = request.form.get('trigger_id')
+	print(trigger_id)
+	Client.views_open(
+	trigger_id=trigger_id,
+	view =
+	 {
+		"type": "modal",
+		"title": {
+			"type": "plain_text",
+			"text": "Remind me to drink water",
+			"emoji": True
+		},
+		"submit": {
+			"type": "plain_text",
+			"text": "Submit",
+			"emoji": True
+		},
+		"close": {
+			"type": "plain_text",
+			"text": "Cancel",
+			"emoji": True
+		},
+		"blocks": [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": "*Let waterbot remind you to drink water :smile:*"
+				}
+			},
+			{
+				"type": "divider"
+			},
+			{
+				"type": "input",
+				"element": {
+					"type": "plain_text_input",
+					"placeholder": {
+						"type": "plain_text",
+						"text": "eg. 10-19",
+						"emoji": True
+					},
+					"action_id":"workinghours"
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "Enter Working Hours (24 Hours)",
+					"emoji": True
+				},
+				"block_id":"workinghours"
+			},
+			{
+				"type": "divider"
+			},
+			{
+				"type": "input",
+				"label": {
+					"type": "plain_text",
+					"text": "Select days to be reminded on",
+					"emoji": True
+				},
+				"block_id":"days",
+				"element": {
+					"type": "multi_static_select",
+					"placeholder": {
+						"type": "plain_text",
+						"text": "Select days",
+						"emoji": True
+					},
+					"action_id":"days",
+					"options": [
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Saturday",
+								"emoji": True
+							},
+							"value": "saturday"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Sunday",
+								"emoji": True
+							},
+							"value": "sunday"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Monday",
+								"emoji": True
+							},
+							"value": "monday"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Tuesday",
+								"emoji": True
+							},
+							"value": "tuesday"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Wednesday",
+								"emoji": True
+							},
+							"value": "wednesday"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Thursday",
+								"emoji": True
+							},
+							"value": "thursday"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Friday",
+								"emoji": True
+							},
+							"value": "friday"
+						}
+					]
+				}
+			},
+			{
+				"type": "divider"
+			},
+			{
+				"type": "input",
+				"element": {
+					"type": "static_select",
+					"placeholder": {
+						"type": "plain_text",
+						"text": "Select duration",
+						"emoji": True
+					},
+					"action_id":"duration",
+					"options": [
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "30 minutes",
+								"emoji": True
+							},
+							"value": "minute30"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "1 hour",
+								"emoji": True
+							},
+							"value": "hour1"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "4 hours",
+								"emoji": True
+							},
+							"value": "hour2"
+						}
+					]
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "Every..",
+					"emoji": True
+				},
+				"block_id":"duration"
+			}
+		]
+	 }
+	)
+	return "",200
 
-    views = {
-        [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Let waterbot remind you to drink water :smile:*"
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "input",
-            "element": {
-                "type": "plain_text_input",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "eg. 10-19",
-                    "emoji": True
-                }
-            },
-            "label": {
-                "type": "plain_text",
-                "text": "Enter Working Hours (24 Hours)",
-                "emoji": True
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "input",
-            "label": {
-                "type": "plain_text",
-                "text": "Select days to be reminded on",
-                "emoji": True
-            },
-            "element": {
-                "type": "multi_static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "Select days",
-                    "emoji": True
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Saturday",
-                            "emoji": True
-                        },
-                        "value": "value-0"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Sunday",
-                            "emoji": True
-                        },
-                        "value": "value-1"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Monday",
-                            "emoji": True
-                        },
-                        "value": "value-2"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Tuesday",
-                            "emoji": True
-                        },
-                        "value": "value-3"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Wednesday",
-                            "emoji": True
-                        },
-                        "value": "value-4"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Thursday",
-                            "emoji": True
-                        },
-                        "value": "value-5"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Friday",
-                            "emoji": True
-                        },
-                        "value": "value-2"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "input",
-            "element": {
-                "type": "static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "Select duration",
-                    "emoji": True
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "30 minutes",
-                            "emoji": True
-                        },
-                        "value": "value-0"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "1 hour",
-                            "emoji": True
-                        },
-                        "value": "value-1"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "4 hours",
-                            "emoji": True
-                        },
-                        "value": "value-2"
-                    }
-                ]
-            },
-            "label": {
-                "type": "plain_text",
-                "text": "Every..",
-                "emoji": True
-            }
-        }
-        ] 
-    }
 
+if __name__ == "__main__":
+  app.run(port=3000)
